1d605e4daed4d73cb0a236081d98ec57c05ff68d
Kevin Mauricio Ramos Pleitez <kramosp3@miumg.edu.gt>
2022-08-10 10:26:35 -0600

    ADD: ejercicio 2 done

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,2 +1,19 @@
+class Operar:
+    
+    def __init__(self, carne, nombre, facultad, numero) -> None:
+        self._carne = carne
+        self._nombre = nombre
+        self._facultad = facultad
+        self._numero = numero
+
+    def datos(self):
+        return f"Carne: {self._carne}, Nombre: {self._nombre}, Facultad: {self._facultad}" 
+
+    def __str__(self) -> str:
+        return f"Carne: {self._carne}, Nombre: {self._nombre}, Facultad: {self._facultad}, Numero: {self._numero}" 
+    
+
 if __name__ == "__main__":
-    print("Running...")
+    operar = Operar(201801234, "Juan Perez", "Ingenieria", 12345678)
+    print(operar.datos())
+    print(operar)
