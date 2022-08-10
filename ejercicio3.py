fe046ba93d95600d253b81a66781e7247f1382d2
Kevin Mauricio Ramos Pleitez <kramosp3@miumg.edu.gt>
2022-08-10 10:18:02 -0600

    ADD: ejercicio 3 done

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,2 +1,18 @@
+import math
+
+
+class Cono:
+    def __init__(self, radio, altura):
+        self.radio = radio
+        self.altura = altura
+
+    def volumen(self):
+        return (1/3) * math.pi * self.radio**2 * self.altura
+
+    def area(self):
+        return math.pi * self.radio * (self.radio + (self.altura**2 + self.radio**2)**(1/2))
+
 if __name__ == "__main__":
-    print("Running...")
+    cono = Cono(2, 3)
+    print("Volumen: ",cono.volumen())
+    print("Area: ",cono.area())
